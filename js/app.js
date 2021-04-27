6b90bcc1a12a2865f9c1caa06ed23742b2bc6b65
gabrielpenhazup <gabrielpenha@zup.com.br>
2021-04-27 17:29:38 -0300

    fixed issues noted in the pull request comments

--- js/app.js
+++ js/app.js
@@ -1,44 +1,60 @@
-const lockers = [{
-    type: 'Categoria1',
-    content: [
-        "xxxxx 1",
-        "xxxxx 2"
-    ]
-},
-{
-    type: 'Categoria2',
-    content: [
-        "xxxxx 1",
-        "xxxxx 2",
-        "xxxxx 3",
-    ]
-},
+const lockers = [
+    {
+        type: 'Categoria1',
+        content: [
+            "xxxxx 1",
+            "xxxxx 2"
+        ]
+    },
+    {
+        type: 'Categoria2',
+        content: [
+            "xxxxx 1",
+            "xxxxx 2",
+            "xxxxx 3",
+        ]
+    },
 
-{
-    type: 'Categoria1',
-    content: [
-        "xxxxx 1",
-        "xxxxx 3"
-    ]
-},
-{
-    type: 'Categoria1',
-    content: [
-        "xxxxx 4",
-    ]
-},
-{
-    type: 'Categoria2',
-    content: [
-        "xxxxx 3",
-    ]
-},
-{
-    type: 'Categoria3',
-    content: [
-        "xxx 1",
-    ]
-},
+    {
+        type: 'Categoria1',
+        content: [
+            "xxxxx 1",
+            "xxxxx 3"
+        ]
+    },
+    {
+        type: 'Categoria1',
+        content: [
+            "xxxxx 4",
+            "xxxxx 5",
+            "xxxxx 6",
+            "xxxxx 7",
+            "xxxxx 8",
+            "xxxxx 9",
+            "xxxxx 10",
+            "xxxxx 11",
+            "xxxxx 12",
+            "xxxxx 13",
+            "xxxxx 14",
+            "xxxxx 15",
+            "xxxxx 16",
+            "xxxxx 17",
+            "xxxxx 18",
+            "xxxxx 19",
+        ]
+    },
+    {
+        type: 'Categoria2',
+        content: [
+            "xxxxx 3",
+        ]
+    },
+    {
+        type: 'Categoria3',
+        content: [
+            "xxx 1",
+        ]
+    },
 ]
 
 const result = [...new Set(lockers.map(typeFiltered => typeFiltered.type))]
@@ -53,12 +69,21 @@ const result = [...new Set(lockers.map(typeFiltered => typeFiltered.type))]
         }
     })
 
-var listContent = result
-    .map(function (item, indice) {
-        return [
-            '<tr><td>' + item.type + '</td></tr>',
-            '<tr><td>' + item.content + '</td></tr>'
-        ]
+const listContent = result
+    .map((item) => {
+        return (
+            `<thead>
+                <tr>
+                    <th>${item.type}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>${item.content}</td>
+                </tr>
+            </tbody>
+            `
+        )
     })
 
-document.getElementById('content').innerHTML = listContent;
+document.getElementById('content').innerHTML = listContent.join('');
